521676d979fc978883dbb8a60d31694821b60958
icast016 <icast016@ucr.edu>
2023-06-13 17:46:41 -0700

    comment

--- naive/convex-hull/main.cpp
+++ naive/convex-hull/main.cpp
@@ -14,6 +14,7 @@ struct Point
 
 int side(Point p, Point q, Point r)
 {
+    // Cross product
     int val = (q.y - p.y) * (r.x - q.x) -
               (q.x - p.x) * (r.y - q.y);
     if (val == 0) return 0;  // Collinear to p->q
